7d9e7420f61a9d9501f8ba858f4a1d4e04e8339a
mdemcode <mdemcode@gmail.com>
2020-08-19 18:18:24 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1 {
+    class Program {
+
+        static void Main(string[] args) {
+            Console.WriteLine("                *****************************");
+            Console.WriteLine("                *                           *");
+            Console.WriteLine("                *     Szyfrowanie HCoIL     *");
+            Console.WriteLine("                *   (c) Michał Demiańczuk   *");
+            Console.WriteLine("                *            2020           *");
+            Console.WriteLine("                *                           *");
+            Console.WriteLine("                *****************************");
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+
+    }
+}
